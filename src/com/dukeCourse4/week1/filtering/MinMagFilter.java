061df82c87fc8c733db9c9d8fdea6295082b6be7
christian.rua@gmail.com <christian.rua@gmail.com>
2022-12-11 19:58:10 -0500

    adding week1 content

---
+++ src/com/dukeCourse4/week1/filtering/MinMagFilter.java
@@ -0,0 +1,19 @@
+package com.dukeCourse4.week1.filtering;
+/**
+ * Write a description of class MinMaxFilter here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MinMagFilter implements Filter{
+    private double magMin;
+
+    public MinMagFilter(double min) {
+        magMin = min;
+    }
+
+    public boolean satisfies(QuakeEntry qe) {
+        return qe.getMagnitude() >= magMin;
+    }
+
+}
